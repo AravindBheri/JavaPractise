cdff1887d2ca99e8d8709c9f7f9b22511c15221a
AravindBheri <65027631+AravindBheri@users.noreply.github.com>
2020-11-03 17:27:56 +0530

    init

---
+++ GUIswing/FlowLayouts/src/Main.java
@@ -0,0 +1,36 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        frame.setSize(400, 400);
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(250, 250));
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(new FlowLayout());
+
+        panel.add(new Button("1"));
+        panel.add(new Button("2"));
+        panel.add(new Button("3"));
+        panel.add(new Button("4"));
+        panel.add(new Button("5"));
+        panel.add(new Button("6"));
+        panel.add(new Button("7"));
+        panel.add(new Button("8"));
+        panel.add(new Button("9"));
+
+        frame.add(panel);
+        frame.setVisible(true);
+        
+    }
+}
