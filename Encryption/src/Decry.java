9b48b42121e2a9524ac6a933253d2eb1b257615c
AravindBheri <65027631+AravindBheri@users.noreply.github.com>
2020-11-03 10:40:18 +0530

    init

---
+++ Encryption/src/Decry.java
@@ -0,0 +1,18 @@
+public class Decry {
+    public static void main(String[] args) throws Exception {
+        int key = 6;
+
+        String text = "Yzgxz&znk&Gzzgiq'";
+
+        System.out.println(text);
+
+        char[] chars = text.toCharArray();
+
+        for (char c : chars) {
+
+            c -= key;
+            System.out.print(c);
+
+        }
+    }
+}
---
+++ Encryption/src/Encry.java
@@ -0,0 +1,18 @@
+public class Encry {
+    public static void main(String[] args) throws Exception {
+        int key = 6;
+
+        String text = "Start the Attack!";
+
+        System.out.println(text);
+
+        char[] chars = text.toCharArray();
+
+        for (char c : chars) {
+
+            c += key;
+            System.out.print(c);
+
+        }
+    }
+}
